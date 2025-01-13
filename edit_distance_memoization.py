e12c877771645f1a3fc8329e9a3c204824eec850
Sivakumaran M <mskmss1516@gmail.com>
2025-01-13 17:40:21 +0530

    Edit Distance Memoization

--- edit_distance_memoization.py
+++ edit_distance_memoization.py
@@ -1,4 +1,5 @@
-"""Edit Distance
+"""
+Edit Distance
 Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
 
 You have the following three operations permitted on a word:
@@ -24,6 +25,8 @@ def minDistance(word1, word2):
     n = len(word1)
     m = len(word2)
     
+    dp=[[-1]*m for _ in range(n)]
+    
     def helper(index1,index2):
         #base case 
         if index1>n-1 and index2>m-1:
@@ -33,14 +36,17 @@ def minDistance(word1, word2):
         if index2>m-1:
             return n-index1
         
+        if dp[index1][index2]!=-1:
+            return dp[index1][index2]
         #recursive case
         if word1[index1]==word2[index2]:
-            return helper(index1+1,index2+1)
-        replace=1+helper(index1+1,index2+1)
-        delete=1+helper(index1+1,index2)
-        insert=1+helper(index1,index2+1)
-        return min(replace,delete,insert)
-        
+            dp[index1][index2]=helper(index1+1,index2+1)
+        else:
+            replace=1+helper(index1+1,index2+1)
+            delete=1+helper(index1+1,index2)
+            insert=1+helper(index1,index2+1)
+            dp[index1][index2]= min(replace,delete,insert)
+        return dp[index1][index2]
         
     return helper(0,0)
 
