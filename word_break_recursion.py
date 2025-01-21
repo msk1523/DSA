f3d43b118e5f9e86f569d1aa9c729b2f5e327c5b
Sivakumaran M <mskmss1516@gmail.com>
2025-01-21 18:23:59 +0530

    Word Break Recursion

---
+++ word_break_recursion.py
@@ -0,0 +1,29 @@
+""" 
+Word Break
+Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+Example :
+Input: s = "jackson", wordDict = ["jack","son"]
+Output: true
+Explanation: Return true because "jackson" can be segmented as "jack son".
+"""
+
+def wordBreak(s, wordDict):
+    n = len(s)
+    dp = [False]*n 
+    
+    def check_ending_at(index):
+        #base case 
+        if index<0: return True
+        
+        #recursive case
+        for word in wordDict:
+            if s[index-len(word)+1:index+1] == word and check_ending_at(index-len(word)):
+                return True
+        return False
+        
+        
+    return check_ending_at(n-1)
+
+print(wordBreak("jackson",["jack","son"]))
