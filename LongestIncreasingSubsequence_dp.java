736c10cad78b0c1d7fdd7ca179a7a499d93772c4
Sivakumaran M <mskmss1516@gmail.com>
2025-09-01 21:27:11 +0530

    Longest increasing subsequence using dp - o(n^2) time complexity

---
+++ LongestIncreasingSubsequence_dp.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class LongestCommonSubsequence_dp {
+    public int Solution(int[] nums){
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp,1);
+        int max=1;
+        for(int i=1; i<nums.length;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j]){
+                    dp[i]=Math.max(dp[i], 1+dp[j]);
+                    max=Math.max(max, dp[i]);
+                }
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int arr1[]={10,9,2,5,3,7,101,18};
+        System.out.println(new LongestCommonSubsequence_dp().Solution(arr1));
+    }
+}
